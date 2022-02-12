4da9339a5f20c19f30f57443e32206931aa10cec
David Cerniglia <dcerniglia@thedevshop.com>
2022-02-12 01:16:48 -0500

    too much for one commit

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+	return (
+		<header>
+			<div className="container">
+				<header className="pt-4">
+					<h1 className="font-rubik text-7xl text-center">
+						actual; expected;
+					</h1>
+					<h2 className="text-center mt-4 font-dosis text-4xl">
+						THIS IS ONLY A TEST
+					</h2>
+				</header>
+			</div>
+		</header>
+	)
+}
---
+++ posts/test.md
@@ -0,0 +1,12 @@
+---
+title: 'Test post'
+date: '22 February 2022'
+excerpt: 'This is an excerpt'
+image: '../public/img/posts'
+---
+
+## This is a Post Title
+
+* list item
+* another item
+* and one more
